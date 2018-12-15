03dbe94ce4f0ecf6946165322c31a8da26b8fca5
Pranjal Satija <me@pranj.co>
2018-12-15 02:06:25 -0600

    add CachingFutureBuilder; improve screens/people and begin implementing screens/new_split

--- lib/screens/new_split.dart
+++ lib/screens/new_split.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:splitty/assets/strings.dart';
+import 'package:splitty/models/item.dart';
+import 'package:splitty/models/split.dart';
+import 'package:splitty/widgets/_index.dart';
 
 import 'main_tab.dart';
 
-class NewSplitScreen extends StatelessWidget implements BottomNavigationBarScreen {
+class NewSplitScreen extends StatefulWidget implements BottomNavigationBarScreen {
   BottomNavigationBarItem bottomNavigationBarItem(BuildContext context) {
     return BottomNavigationBarItem(
       icon: Icon(Icons.add),
@@ -12,10 +15,63 @@ class NewSplitScreen extends StatelessWidget implements BottomNavigationBarScree
   }
 
   @override
+  State<StatefulWidget> createState() => _NewSplitScreenState();
+}
+
+class _NewSplitScreenState extends State<NewSplitScreen> {
+  Future<Split> _currentSplitFuture;
+  Split _currentSplit;
+
+  _NewSplitScreenState() {
+    _currentSplitFuture = SplitController.currentSplit();
+  }
+
+  void _addItem() async {
+    print('Implement me!');
+    // TODO: Implement
+  }
+
+  Widget _buildItemListTile(Item item) {
+    return ListTile(
+      title: Text(item.name),
+      subtitle: Text(item.formattedDescription),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.of(context).newSplit),
+      ),
+      body: CachingFutureBuilder<Split>(
+        future: _currentSplitFuture,
+        cachedData: _currentSplit,
+        cacheSaver: (currentSplit) => _currentSplit = currentSplit,
+        loadingWidgetBuilder: (context) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        finishedWidgetBuilder: (context, currentSplit, error) {
+          if (currentSplit == null || currentSplit.items.isEmpty) {
+            return ListViewEmptyState(
+              actionText: Strings.of(context).addItemPrompt,
+              message: Strings.of(context).addItemMessage,
+              onPressed: () => _addItem(),
+            );
+          } else {
+            return ListView.separated(
+              itemCount: currentSplit.items.length,
+              itemBuilder: (context, index) => _buildItemListTile(currentSplit.items[index]),
+              separatorBuilder: (context, index) => Divider(height: 1.0),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _addItem(),
       ),
     );
   }
