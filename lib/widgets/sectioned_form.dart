2bf8f37bc7d8c1556bdca91766c3b2fd04285325
Pranjal Satija <me@pranj.co>
2019-01-02 11:27:43 -0500

    improve SectionedForm

--- lib/widgets/sectioned_form.dart
+++ lib/widgets/sectioned_form.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SectionedForm extends StatelessWidget {
-  EdgeInsetsGeometry padding;
-  List<FormSection> sections;
+  final EdgeInsetsGeometry padding;
+  final List<FormSection> sections;
 
   SectionedForm({
-    this.padding = const EdgeInsets.all(16),
+    this.padding = const EdgeInsets.only(bottom: 16),
     this.sections,
   });
 
@@ -21,29 +21,37 @@ class SectionedForm extends StatelessWidget {
 }
 
 class FormSection extends StatelessWidget {
-  final String title;
   final Widget body;
+  final EdgeInsetsGeometry padding;
+  final String title;
 
-  FormSection({this.title, this.body});
+  FormSection({
+    this.body,
+    this.padding = const EdgeInsets.only(top: 16, left: 16, right: 16),
+    this.title,
+  });
 
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(title,
-          style: Theme.of(context).textTheme.headline,
-        ),
-        Container(height: 4),
-        Container(
-          child: body,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            border: Border.all(
-              color: Theme.of(context).dividerColor,
+    return Padding(
+      padding: padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(title,
+            style: Theme.of(context).textTheme.headline,
+          ),
+          Container(height: 4),
+          Container(
+            child: body,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              border: Border.all(
+                color: Theme.of(context).dividerColor,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
